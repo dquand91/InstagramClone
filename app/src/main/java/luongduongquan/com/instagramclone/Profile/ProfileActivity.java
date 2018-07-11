fee1f9790414b75004b6d6358642fbb54f6b5fd0
dquand91 <dquand91@gmail.com>
2018-07-11 16:43:21 +0700

    - Create layout for Profile Activity

--- app/src/main/java/luongduongquan/com/instagramclone/Profile/ProfileActivity.java
+++ app/src/main/java/luongduongquan/com/instagramclone/Profile/ProfileActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,10 +23,28 @@ public class ProfileActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_home);
+		setContentView(R.layout.activity_profile);
 		Log.d(TAG, "onCreate: ");
 		setupBottomNavigationView();
+		setupToolbar();
+	}
 
+	private void setupToolbar(){
+		android.support.v7.widget.Toolbar toolbar = findViewById(R.id.profileToolBar);
+		setSupportActionBar(toolbar);
+
+		toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+			@Override
+			public boolean onMenuItemClick(MenuItem item) {
+				Log.d(TAG, "onMenuItemClick: clicked menu item: " + item);
+				switch (item.getItemId()){
+					case R.id.profileMenu_item:
+						Log.d(TAG, "onMenuItemClick: Navigation to Profile Preference,");
+					
+				}
+				return false;
+			}
+		});
 	}
 
 	private void setupBottomNavigationView(){
@@ -39,5 +58,13 @@ public class ProfileActivity extends AppCompatActivity {
 		Menu menu = bottomNavigationViewEx.getMenu();
 		MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
 		menuItem.setChecked(true);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		getMenuInflater().inflate(R.menu.profile_menu, menu);
+
+		return true;
 	}
 }
