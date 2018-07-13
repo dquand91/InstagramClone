a4b0a8bb005f3c74fbf15b365d6a301983c8218b
dquand91 <dquand91@gmail.com>
2018-07-13 12:52:38 +0700

    - Login activity

--- app/src/main/java/luongduongquan/com/instagramclone/Likes/LikesActivity.java
+++ app/src/main/java/luongduongquan/com/instagramclone/Likes/LikesActivity.java
@@ -22,9 +22,9 @@ public class LikesActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_home);
+		setContentView(R.layout.activity_login);
 		Log.d(TAG, "onCreate: ");
-		setupBottomNavigationView();
+//		setupBottomNavigationView();
 
 	}
 
