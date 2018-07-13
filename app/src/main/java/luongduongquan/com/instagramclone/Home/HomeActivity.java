5157d66c490a440151c4992542c9be3d5a737e6f
dquand91 <dquand91@gmail.com>
2018-07-13 15:05:39 +0700

    Login function

--- app/src/main/java/luongduongquan/com/instagramclone/Home/HomeActivity.java
+++ app/src/main/java/luongduongquan/com/instagramclone/Home/HomeActivity.java
@@ -1,7 +1,9 @@
 package luongduongquan.com.instagramclone.Home;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -9,9 +11,12 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
+import luongduongquan.com.instagramclone.Login.LoginActivity;
 import luongduongquan.com.instagramclone.R;
 import luongduongquan.com.instagramclone.Utils.BottomNavigationViewHelper;
 import luongduongquan.com.instagramclone.Utils.UniversalImageLoader;
@@ -23,11 +28,18 @@ public class HomeActivity extends AppCompatActivity {
 
 	private Context mContext = HomeActivity.this;
 
+	//firebase
+	private FirebaseAuth mAuth;
+	private FirebaseAuth.AuthStateListener mAuthListener;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
 		Log.d(TAG, "onCreate: ");
+
+		setupFirebaseAuth();
+
 		initImageLoader();
 
 		setupBottomNavigationView();
@@ -77,4 +89,69 @@ public class HomeActivity extends AppCompatActivity {
 		MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
 		menuItem.setChecked(true);
 	}
+
+
+	 /*
+    ------------------------------------ Firebase ---------------------------------------------
+     */
+
+	/**
+	 * checks to see if the @param 'user' is logged in
+	 * @param user
+	 */
+	private void checkCurrentUser(FirebaseUser user){
+		Log.d(TAG, "checkCurrentUser: checking if user is logged in.");
+
+		if(user == null){
+			Log.d(TAG, "Did not login");
+			Intent intent = new Intent(mContext, LoginActivity.class);
+			startActivity(intent);
+		}
+	}
+	/**
+	 * Setup the firebase auth object
+	 */
+	private void setupFirebaseAuth(){
+		Log.d(TAG, "setupFirebaseAuth: setting up firebase auth.");
+
+		mAuth = FirebaseAuth.getInstance();
+
+		mAuthListener = new FirebaseAuth.AuthStateListener() {
+			@Override
+			public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+				FirebaseUser user = firebaseAuth.getCurrentUser();
+
+				//check if the user is logged in
+				checkCurrentUser(user);
+
+				if (user != null) {
+					// User is signed in
+					Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+				} else {
+					// User is signed out
+					Log.d(TAG, "onAuthStateChanged:signed_out");
+				}
+				// ...
+			}
+		};
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		mAuth.addAuthStateListener(mAuthListener);
+//		checkCurrentUser(mAuth.getCurrentUser());
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		if (mAuthListener != null) {
+			mAuth.removeAuthStateListener(mAuthListener);
+		}
+	}
+
+	 /*
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^ Firebase ^^^^^^^^^^^^^^^^^^^^^^^^^^
+     */
 }
--- app/src/main/java/luongduongquan/com/instagramclone/Likes/LikesActivity.java
+++ app/src/main/java/luongduongquan/com/instagramclone/Likes/LikesActivity.java
@@ -22,9 +22,9 @@ public class LikesActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_register);
+		setContentView(R.layout.activity_home);
 		Log.d(TAG, "onCreate: ");
-//		setupBottomNavigationView();
+		setupBottomNavigationView();
 
 	}
 
