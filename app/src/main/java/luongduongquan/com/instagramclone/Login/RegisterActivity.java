43c3fe4f96dbc79ce5a234787a823a85a9c4cc88
dquand91 <dquand91@gmail.com>
2018-07-16 09:57:56 +0700

    - Update Register new user and add to FireBase DataBase (part.26 - File.28)

--- app/src/main/java/luongduongquan/com/instagramclone/Login/RegisterActivity.java
+++ app/src/main/java/luongduongquan/com/instagramclone/Login/RegisterActivity.java
@@ -153,6 +153,10 @@ public class RegisterActivity extends AppCompatActivity {
                             username = username + append;
 
                             //add new user to the database
+                            firebaseMethods.addNewUser(email, username, "", "", "");
+
+                            Toast.makeText(mContext, "Signup successful. Sending verification email.", Toast.LENGTH_SHORT).show();
+
 
                             //add new user_account_settings to the database
                         }
--- app/src/main/java/luongduongquan/com/instagramclone/Utils/FirebaseMethods.java
+++ app/src/main/java/luongduongquan/com/instagramclone/Utils/FirebaseMethods.java
@@ -10,9 +10,12 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import luongduongquan.com.instagramclone.R;
 import luongduongquan.com.instagramclone.models.User;
+import luongduongquan.com.instagramclone.models.UserAccountSettings;
 
 
 /**
@@ -28,11 +31,17 @@ public class FirebaseMethods {
     private FirebaseAuth.AuthStateListener mAuthListener;
     private String userID;
 
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+
     private Context mContext;
 
     public FirebaseMethods(Context context) {
         mAuth = FirebaseAuth.getInstance();
         mContext = context;
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
 
         if(mAuth.getCurrentUser() != null){
             userID = mAuth.getCurrentUser().getUid();
@@ -74,7 +83,7 @@ public class FirebaseMethods {
 
         User user = new User();
 
-        for (DataSnapshot ds: datasnapshot.getChildren()){
+        for (DataSnapshot ds: datasnapshot.child(userID).getChildren()){
             Log.d(TAG, "checkIfUsernameExists: datasnapshot: " + ds);
 
             user.setUsername(ds.getValue(User.class).getUsername());
@@ -86,5 +95,32 @@ public class FirebaseMethods {
             }
         }
         return false;
+    }
+
+
+    public void addNewUser(String email, String username, String description, String website, String profile_photo){
+
+        User user = new User( userID,  1,  email,  StringManipulation.condenseUsername(username) );
+
+        myRef.child(mContext.getString(R.string.dbname_users))
+                .child(userID)
+                .setValue(user);
+
+
+        UserAccountSettings settings = new UserAccountSettings(
+                description,
+                username,
+                0,
+                0,
+                0,
+                profile_photo,
+                username,
+                website
+        );
+
+        myRef.child(mContext.getString(R.string.dbname_user_account_settings))
+                .child(userID)
+                .setValue(settings);
+
     }
 }
